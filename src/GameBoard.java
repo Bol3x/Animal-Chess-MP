bd8aa85c9dce6546769a8baad3e42e434148d27a
Shaneuy <shane_owen_uy@dlsu.edu.ph>
2021-07-17 03:53:12 +0800

    Update GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -18,7 +18,11 @@ public class GameBoard {
      * @author Carlo
      */
     public void initPlayBoard(){
-
+        
+        //set players
+        players[0] = new Player(Terrain.DEN1);
+        players[1] = new Player(Terrain.DEN2);
+        
         //set dens
         playBoard[0][3] = new Tile(new Position(0,3) , Terrain.DEN1);
         playBoard[ROW-1][3] = new Tile(new Position(ROW-1,3) , Terrain.DEN2);
@@ -30,7 +34,7 @@ public class GameBoard {
         playBoard[8][2] = new Tile(new Position(8,2) , Terrain.TRAP);
         playBoard[8][4] = new Tile(new Position(8,4) , Terrain.TRAP);
         playBoard[7][3] = new Tile(new Position(7,3) , Terrain.TRAP);
-
+                
         //set other tiles
         for(int i = 0; i < ROW; i++){
             for(int j = 0; j < COL; j++){
@@ -42,6 +46,56 @@ public class GameBoard {
                     playBoard[i][j] = new Tile(new Position(i,j));
             }
         }
+        
+          //set Player 1 Animals
+         Animal mouse = new Animal(players[0], 1, "Mouse", new Position(2,0));
+         playBoard[2][0].setAnimal(mouse);
+         
+         Animal cat = new Animal(players[0], 2, "Cat", new Position(1,5));
+         playBoard[1][5].setAnimal(cat);
+         
+         Animal wolf = new Animal(players[0], 3, "Wolf", new Position(2,4));
+         playBoard[2][4].setAnimal(wolf);
+         
+         Animal dog = new Animal(players[0], 4, "Dog", new Position(1,1));
+         playBoard[1][1].setAnimal(dog);
+         
+         Animal leopard = new Animal(players[0], 5, "Leopard", new Position(2,2));
+         playBoard[2][2].setAnimal(leopard);
+                         
+         Animal tiger = new Animal(players[0], 6, "Tiger", new Position(0,6));
+         playBoard[0][6].setAnimal(tiger); 
+         
+         Animal lion = new Animal(players[0], 7, "Lion", new Position(0,0));
+         playBoard[0][0].setAnimal(lion);
+                                 
+         Animal elephant = new Animal(players[0], 8, "Elephant", new Position(2,6));
+         playBoard[2][6].setAnimal(elephant);
+         
+         //set Player 2 Animals
+         Animal mouse2 = new Animal(players[1], 1, "Mouse2", new Position(6,6));
+         playBoard[6][6].setAnimal(mouse2);
+         
+         Animal cat2 = new Animal(players[1], 2, "Cat2", new Position(7,1));
+         playBoard[7][1].setAnimal(cat2);
+         
+         Animal wolf2 = new Animal(players[1], 3, "Wolf2", new Position(6,2));
+         playBoard[6][2].setAnimal(wolf2);
+         
+         Animal dog2 = new Animal(players[1], 4, "Dog2", new Position(7,5));
+         playBoard[7][5].setAnimal(dog2);
+         
+         Animal leopard2 = new Animal(players[1], 5, "Leopard2", new Position(6,4));
+         playBoard[6][4].setAnimal(leopard2);
+                         
+         Animal tiger2 = new Animal(players[1], 6, "Tiger2", new Position(8,0));
+         playBoard[8][0].setAnimal(tiger2); 
+         
+         Animal lion2 = new Animal(players[1], 7, "Lion2", new Position(8,6));
+         playBoard[8][6].setAnimal(lion2);
+                                 
+         Animal elephant2 = new Animal(players[1], 8, "Elephant2", new Position(6,0));
+         playBoard[6][0].setAnimal(elephant2);
     }
 
     /**Constructor*/
@@ -75,7 +129,7 @@ public class GameBoard {
      * @param currAnimal current animal to move
      * @param newPos new position to check
      * 
-     * @return true if successful, false if unsuccesful
+     * @return true if successful, false if unsuccessful
      * @author Carlo
      */
     public boolean moveAnimal(Animal currAnimal, Position newPos){
