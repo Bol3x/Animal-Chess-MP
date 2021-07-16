e7a080fa780b1b0938228ff022bc394936c7c25f
Shaneuy <shane_owen_uy@dlsu.edu.ph>
2021-07-16 17:33:16 +0800

    Update Position.java

--- src/Position.java
+++ src/Position.java
@@ -2,4 +2,41 @@ package src;
 
 public class Position {
     
+    private final int X; 
+    private final int Y;
+    
+    public Position(int x, int y) {
+        this.X = x;
+        this.Y = y;
+    }
+            
+    //** Getters */ 
+    public int getX() {
+        return this.X;
+    }
+    
+    public int getY() {
+        return this.Y;
+    }
+ 
+    public static boolean isWithinBounds (Position pos) {
+        return pos.X < ROW && pos.Y < COL;
+    }
+     
+    @Override
+    public boolean equals (Object obj) {
+        if(obj != null) {
+            Position otherPosition = (Position) obj;
+            return this.X == otherPosition.X && this.Y == otherPosition.Y;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.X;
+        hash = 97 * hash + this.Y;
+        return hash;
+    }
 }
