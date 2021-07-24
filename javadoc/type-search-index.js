3aa98c95591dabddd09e8d7acad61d4803130615
Bol3x <carloroleda@gmail.com>
2021-07-24 18:49:56 +0800

    Sample of javadoc page

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"src","l":"Animal"},{"p":"src","l":"GameBoard"},{"p":"src","l":"Player"},{"p":"src","l":"Position"},{"p":"src","l":"Terrain"},{"p":"driver","l":"TestMain"},{"p":"src","l":"Tile"}];updateSearchResults();
