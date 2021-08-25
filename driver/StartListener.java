68bad6a8bdc515b7778518690943562c84c73599
Shaneuy <shane_owen_uy@dlsu.edu.ph>
2021-08-26 03:08:47 +0800

    Update StartListener.java

--- driver/StartListener.java
+++ driver/StartListener.java
@@ -1,15 +1,17 @@
 package driver;
 
 import java.awt.event.*;
+import javax.swing.JButton;
 
-import view.PlayerSelectPanel;
+import view.PlayerPanel;
 
 public class StartListener implements ActionListener{
     int[] nChoices;
     int nCurrent;
-    PlayerSelectPanel view;
-
-    public StartListener(PlayerSelectPanel menuPanel){
+    PlayerPanel view;
+    int i = 1;
+    
+    public StartListener(PlayerPanel menuPanel){
         nChoices = new int[2];
         nCurrent = 0;
         view = menuPanel;
@@ -17,14 +19,21 @@ public class StartListener implements ActionListener{
     }
 
     public void actionPerformed(ActionEvent e){
+         nChoices[nCurrent] = Integer.parseInt(e.getActionCommand());
+       
         
-        nChoices[nCurrent] = Integer.parseInt(e.getActionCommand());
-        view.setResult("" + nChoices[nCurrent]);
+        view.setResult("Player " + i + " chose number "  + nChoices[nCurrent]);
+        i++;
         nCurrent++;
-
+       
         if(nCurrent == 2){
-            view.disableChoiceButtons();
-            view.enableNextButton();
+              view.disableChoiceButtons();
+              view.enableNextButton();
+              
+              if(nChoices[0] > nChoices[1]) 
+                  view.setResult("Player 1 gets to pick colors");
+              else
+                  view.setResult("Player 2 gets to pick colors");
         }
     }
 }
