f81fb6c9a76d8662fcfcbabde8cf9c08e0c42518
Shaneuy <shane_owen_uy@dlsu.edu.ph>
2021-08-28 02:11:20 +0800

    Update ColorListener.java

--- controller/ColorListener.java
+++ controller/ColorListener.java
@@ -14,7 +14,8 @@ public class ColorListener implements ActionListener {
         view = colorPanel;
         pHandler = handler;
         view.setColorButtonListener(this);
-
+        view.setLabel1Visibility(false);
+        view.setLabel2Visibility(false);
         view.setSelector("Player " + (pHandler.getFirstPlayerIdx()+1) + ", Select a color below:");
     }
     
@@ -28,12 +29,20 @@ public class ColorListener implements ActionListener {
         
         switch(nChoice) 
         {
-            case 1: view.setResult("Player " + (pHandler.getFirstPlayerIdx()+1) + " will be Red and Player " + (pHandler.getSecondPlayerIdx()+1) + " will be Blue");
+            case 1: view.setLabel1Result("Player " + (pHandler.getFirstPlayerIdx()+1) + " will be Red and Player " + (pHandler.getSecondPlayerIdx()+1) + " will be Blue");
+                view.setLabel2Result("Player " + (pHandler.getFirstPlayerIdx() + 1) + " chose Red");
+                view.setLabel2ColorRed();
+                view.setLabel1Visibility(true);
+                view.setLabel2Visibility(true);
                 pHandler.initFirstPlayerColor(AvailableColor.RED);
                 pHandler.initSecondPlayerColor(AvailableColor.BLUE);
             break;
              
-            case 2: view.setResult("Player " + (pHandler.getFirstPlayerIdx()+1) + " will be Blue and Player " + (pHandler.getSecondPlayerIdx()+1) + " will be Red");
+            case 2: view.setLabel1Result("Player " + (pHandler.getFirstPlayerIdx()+1) + " will be Blue and Player " + (pHandler.getSecondPlayerIdx()+1) + " will be Red");
+                view.setLabel2Result("Player " + (pHandler.getFirstPlayerIdx() + 1) + " chose Blue");
+                  view.setLabel2ColorBlue();
+                view.setLabel1Visibility(true);
+                view.setLabel2Visibility(true);   
                 pHandler.initFirstPlayerColor(AvailableColor.BLUE);
                 pHandler.initSecondPlayerColor(AvailableColor.RED);
             break;
