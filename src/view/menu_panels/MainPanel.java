e6dca56981224e4217d699f3113aa98221b0e3af
Shaneuy <shane_owen_uy@dlsu.edu.ph>
2021-09-06 14:22:23 +0800

    Update MainPanel.java

--- src/view/menu_panels/MainPanel.java
+++ src/view/menu_panels/MainPanel.java
@@ -34,7 +34,7 @@ public class MainPanel extends javax.swing.JPanel {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/src/view/images/mion.jpg"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/src/view/images/logo.jpg")));
 
         jButton1.setText("Start Game");
 
